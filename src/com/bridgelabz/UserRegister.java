194cb178eb5ffecdfb6a566585e4a32e1f868f25
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-11-04 21:48:46 +0530

    Added UserRegisterUC1

---
+++ src/com/bridgelabz/UserRegister.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegister {
+    static Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+    public static void main(String[] args) {
+        Matcher matcher = pattern.matcher("Prathmesh");
+        if (matcher.matches()){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+    }
+}
