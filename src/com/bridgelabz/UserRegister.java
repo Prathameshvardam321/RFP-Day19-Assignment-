2bc12946b764563e53057b6052dba5ac81037515
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-11-04 23:02:49 +0530

    Added UserRegisterUC4

--- src/com/bridgelabz/UserRegister.java
+++ src/com/bridgelabz/UserRegister.java
@@ -7,8 +7,9 @@ public class UserRegister {
     static Pattern firstName= Pattern.compile("^[A-Z]{1}[a-z]{2,}");
     static Pattern lastName= Pattern.compile("^[A-Z]{1}[a-z]{2,}");
     static Pattern emailId = Pattern.compile("^[A-Za-z]{3,}[.]?[a-z]*@([a-z]+)[.]?[a-z]+[.]?[a-z]*$");
+    static Pattern countryCode = Pattern.compile("^[0-9]{2}[\\s][0-9]{10}$");
     public static void main(String[] args) {
-        Matcher matcher = emailId.matcher("Prathmesh.vardam@bl.co");
+        Matcher matcher = countryCode.matcher("91 9930516251");
         if (matcher.matches()){
             System.out.println(true);
         } else {
