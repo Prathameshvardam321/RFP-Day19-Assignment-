810406da2308e8c153e9a9ef8b01f445ba5e4645
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-11-04 23:13:14 +0530

    Added UserRegisterUC5

--- src/com/bridgelabz/UserRegister.java
+++ src/com/bridgelabz/UserRegister.java
@@ -8,10 +8,12 @@ public class UserRegister {
     static Pattern lastName= Pattern.compile("^[A-Z]{1}[a-z]{2,}");
     static Pattern emailId = Pattern.compile("^[A-Za-z]{3,}[.]?[a-z]*@([a-z]+)[.]?[a-z]+[.]?[a-z]*$");
     static Pattern countryCode = Pattern.compile("^[0-9]{2}[\\s][0-9]{10}$");
+    static Pattern passwordRule1 = Pattern.compile("^[a-zA-Z]{8,}");
     public static void main(String[] args) {
-        Matcher matcher = countryCode.matcher("91 9930516251");
+        Matcher matcher = passwordRule1.matcher("PassWord");
         if (matcher.matches()){
             System.out.println(true);
+
         } else {
             System.out.println(false);
         }
