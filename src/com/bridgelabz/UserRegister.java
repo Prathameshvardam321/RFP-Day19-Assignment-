2dc6af0f22fb253a5401843929c384aecc844136
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-11-06 19:18:50 +0530

    Added UserRegisterUC9

--- src/com/bridgelabz/UserRegister.java
+++ src/com/bridgelabz/UserRegister.java
@@ -11,8 +11,9 @@ public class UserRegister {
     static Pattern passwordRule2 = Pattern.compile("^(?=.*[A-Z]).{8,}$");
     static Pattern passwordRule3 = Pattern.compile("^(?=.[A-Z])(?=.*[0-9]).{8,}$");
     static Pattern passwordRule4 = Pattern.compile("^(?=.*[A-Z])(?=.*[!@#$%&*()])(?=.*[0-9])(?=\\S+$).{8,}$");
+    static Pattern emailPattern = Pattern.compile("^[a-z]{3,}[0-9]*([+-._]{1}[0-9]+)*@[a-z0-9]+[.]{1}[a-z]{2,}[,]?([.]{1}[a-z]{1,})?");
     public static void main(String[] args) {
-        Matcher matcher = passwordRule4.matcher("papapA1akm");
+        Matcher matcher = emailPattern.matcher("abc@gmail.com.aa");
         if (matcher.matches()){
             System.out.println(true);
         } else {
